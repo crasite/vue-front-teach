b12fb1533c2d008edba75122439713a0bed5d4c3
Thanu Poptiphueng <tpcrasite@gmail.com>
2023-05-10 13:14:16 +0700

    simple component binding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, computed } from "vue";
 import { client } from "./trpc";
+import ShowInfo from "./components/ShowInfo.vue";
 // import { client } from "./trpc";
 const txt = ref("");
 const helloTxt = computed(() => {
@@ -16,7 +17,6 @@ const alertWelcome = () => {
 };
 const addKitten = async (name: string) => {
   const dbKitten = await client.database.addKitten.mutate(name);
-  client.database.hello.query();
   alert(`${dbKitten.name}: ${dbKitten.age}`);
 };
 
@@ -37,12 +37,13 @@ const findKitten = async (name: string) => {
 <template>
   <h1 v-if="shouldShow">Welcome</h1>
   <h1 v-else-if="!shouldShow">Please input your cat's name</h1>
-  <h1 @click="() => alertWelcome()">{{ helloTxt }}</h1>
+  <h1 @click.meta="() => alertWelcome()">{{ helloTxt }}</h1>
   <input type="text" v-model="txt" />
   <button @click="() => addKitten(txt)">Add Kitten</button>
   <button @click="() => findKitten(txt)">Find Kitten</button>
   <p v-if="kitten">{{ `${kitten.name} is ${kitten.age} years old.` }}</p>
   <p v-else-if="txt.length > 0">Kittens not found</p>
+  <ShowInfo :message="txt" />
 </template>
 
 <style scoped></style>
---
+++ src/components/ShowInfo.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps({
+  message: String,
+});
+</script>
+
+<template>
+  <h1>Hello {{ message }}</h1>
+</template>
