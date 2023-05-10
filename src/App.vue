251c1901e6baff61e01b17c7a91a44d05047c899
Thanu Poptiphueng <tpcrasite@gmail.com>
2023-05-10 10:45:24 +0700

    @click event

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,16 @@ const helloTxt = computed(() => {
     return "";
   }
 });
+const shouldShow = computed(() => txt.value.length > 0);
+const alertWelcome = () => {
+  alert("Welcome");
+};
 </script>
 
 <template>
-  <h1>{{ helloTxt }}</h1>
+  <h1 v-if="shouldShow">Welcome</h1>
+  <h1 v-else-if="!shouldShow">Please input your name</h1>
+  <h1 @click="() => alertWelcome()">{{ helloTxt }}</h1>
   <input type="text" v-model="txt" />
 </template>
 
