2d41bd8647a3e488a5686a0bf4b742998a12695b
Thanu Poptiphueng <tpcrasite@gmail.com>
2023-05-10 10:23:44 +0700

    added computed value

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-import { ref } from "vue";
-const msg = ref("Hi");
+import { ref, computed } from "vue";
+const txt = ref("");
+const helloTxt = computed(() => {
+  if (txt.value.length > 0) {
+    return "Hello " + txt.value;
+  } else {
+    return "";
+  }
+});
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ helloTxt }}</h1>
+  <input type="text" v-model="txt" />
 </template>
 
 <style scoped></style>
